fbb9524c2509010a3cc2131444072c8409cad0ca
yyczz <a729746954@gmail.com>
2024-05-11 21:47:08 +0800

    init

--- src/example/EZDN404.sol
+++ src/example/EZDN404.sol
@@ -6,9 +6,17 @@ import "../DN404Mirror.sol";
 import {Ownable} from "solady/auth/Ownable.sol";
 import {LibString} from "solady/utils/LibString.sol";
 import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
-// import {MerkleProofLib} from "solady/utils/MerkleProofLib.sol";
 
 import {SignatureCheckerLib} from "solady/utils/SignatureCheckerLib.sol";
+
+import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol';
+import '../utils/TickMath7.sol';
+// import '@uniswap/v3-core/contracts/libraries/TickMath.sol';
+import '@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol';
+import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
+import '@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol';
+import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
+import '@uniswap/v3-periphery/contracts/base/LiquidityManagement.sol';
 
 /**
  * @title NFTMintDN404
@@ -20,14 +28,12 @@ contract EZDN404 is DN404, Ownable {
     string private _name;
     string private _symbol;
     string private _baseURI;
-    // bytes32 private _allowlistRoot;
-    // uint96 public publicPrice; // uint96 is sufficient to represent all ETH in existence.
-    // uint96 public allowlistPrice; // uint96 is sufficient to represent all ETH in existence.
+
     uint32 public totalMinted; // DN404 only supports up to `2**32 - 2` tokens.
     bool public live;
 
-    uint256 public mintPrice;
-    uint256 public mintPriceInterval;
+    uint256 public pbMintPrice;
+    uint256 public wlMintPrice;
 
     uint32 public constant MAX_PER_WALLET = 5;
     uint32 public constant MAX_SUPPLY = 5000;
@@ -41,8 +47,8 @@ contract EZDN404 is DN404, Ownable {
     constructor(
         string memory name_,
         string memory symbol_,
-        uint256 _mintPrice,
-        uint256 _mintPriceInterval,
+        uint256 _pbMintPrice,
+        uint256 _wlMintPrice,
         uint96 initialTokenSupply,
         address initialSupplyOwner
     ) {
@@ -51,10 +57,10 @@ contract EZDN404 is DN404, Ownable {
         _name = name_;
         _symbol = symbol_;
 
-        mintPrice = _mintPrice;
-        mintPriceInterval = _mintPriceInterval;
+        pbMintPrice = _pbMintPrice;
+        wlMintPrice = _wlMintPrice;
 
-        address mirror = address(new DN404Mirror(msg.sender));
+        address mirror = address(new DN404Mirror(msg.sender)); // erc721
         _initializeDN404(initialTokenSupply, initialSupplyOwner, mirror);
     }
 
@@ -65,22 +71,9 @@ contract EZDN404 is DN404, Ownable {
         _;
     }
 
-    modifier checkPriceAndUpdate(uint256 price, uint256 nftAmount) {
-        uint256 totalMintPrice =
-            price * nftAmount + nftAmount * (nftAmount - 1) * mintPriceInterval / 2; // Sn = a1*n + n(n-1)d/2
-
-        if (totalMintPrice < msg.value) {
+    modifier checkPrice(uint256 price, uint256 nftAmount) {
+        if (price * nftAmount != msg.value) {
             revert InvalidPrice();
-        }
-
-        // update mintPrice
-        uint256 newMintPrice = price + nftAmount * mintPriceInterval;
-        mintPrice = newMintPrice;
-
-        // refund the rest
-        uint256 restFund = msg.value - totalMintPrice;
-        if (restFund > 0) {
-            SafeTransferLib.safeTransferETH(msg.sender, restFund);
         }
         _;
     }
@@ -104,47 +97,30 @@ contract EZDN404 is DN404, Ownable {
         _;
     }
 
-    // function mint(uint256 nftAmount)
-    //     public
-    //     payable
-    //     onlyLive
-    //     checkPriceAndUpdate(publicPrice, nftAmount)
-    //     checkAndUpdateBuyerMintCount(nftAmount)
-    //     checkAndUpdateTotalMinted(nftAmount)
-    // {
-    //     _mint(msg.sender, nftAmount * _unit());
-    // }
-
-    // function allowlistMint(uint256 nftAmount, bytes32[] calldata proof)
-    //     public
-    //     payable
-    //     onlyLive
-    //     checkPriceAndUpdate(allowlistPrice, nftAmount)
-    //     checkAndUpdateBuyerMintCount(nftAmount)
-    //     checkAndUpdateTotalMinted(nftAmount)
-    // {
-    //     bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
-    //     if (!MerkleProofLib.verifyCalldata(proof, _allowlistRoot, leaf)) {
-    //         revert InvalidProof();
-    //     }
-    //     _mint(msg.sender, nftAmount * _unit());
-    // }
-
     function publicMint(uint256 nftAmount)
         public
         payable
         onlyLive
-        checkPriceAndUpdate(mintPrice, nftAmount)
+        checkPrice(pbMintPrice, nftAmount)
         checkAndUpdateBuyerMintCount(nftAmount)
         checkAndUpdateTotalMinted(nftAmount)
-    {}
+    {
+        _mint(msg.sender, nftAmount * _unit());
+    }
 
-    function whitelistMint(uint256 nftAmount, bytes32 signature) public onlyLive {
+    function whitelistMint(uint256 nftAmount, bytes32 signature)
+        public
+        payable
+        onlyLive
+        checkPrice(pbMintPrice, nftAmount)
+        checkAndUpdateBuyerMintCount(nftAmount)
+        checkAndUpdateTotalMinted(nftAmount)
+    {
         // check signature
+        _mint(msg.sender, nftAmount * _unit());
     }
 
     ////////////////
-
     function setBaseURI(string calldata baseURI_) public onlyOwner {
         _baseURI = baseURI_;
     }
